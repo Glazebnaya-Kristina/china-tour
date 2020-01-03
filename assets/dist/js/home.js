392573dead54c8575c97928f113d037d3bb55fab
Glazebnaya-Kristina <46931114+Glazebnaya-Kristina@users.noreply.github.com>
2020-01-03 19:21:14 +0200

    Разметка и стили для секции steps

--- assets/dist/js/home.js
+++ assets/dist/js/home.js
@@ -1,8 +1,42 @@
 $(document).ready(function () {
-   // $('.header__slider').slick({
-   //    arrows: false,
-   //    dots: true
-   // });
+
+   var carouselSteps = $('.steps__carousel');
+
+   carouselSteps.on("init", function (event, slick) {
+      $(".step-count").html(parseInt(slick.currentSlide + 1) + '<span> /' + slick.slideCount + '</span>');
+   });
+
+   carouselSteps.on("afterChange", function (event, slick, currentSlide) {
+      $(".step-count").html(parseInt(slick.currentSlide + 1) + '<span> /' + slick.slideCount + '</span>');
+   });
+
+
+   carouselSteps.slick({
+      prevArrow: $('.steps__arrow--prev'),
+      nextArrow: $('.steps__arrow--next'),
+      dots: false,
+      mobileFirst: true,
+      responsive: [
+
+         {
+            breakpoint: 768,
+            settings: {
+               slidesToShow: 2
+            }
+         },
+         {
+            breakpoint: 1024,
+            settings: {
+               slidesToShow: 3
+            }
+         },
+         {
+            breakpoint: 1430,
+            settings: "unslick"
+         }
+      ]
+   });
+
 });
 
 
