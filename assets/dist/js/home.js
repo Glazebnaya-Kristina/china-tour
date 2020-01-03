15d63f6bcb508edf125400a3cdfb5a557e046ea2
Glazebnaya-Kristina <46931114+Glazebnaya-Kristina@users.noreply.github.com>
2020-01-03 22:22:17 +0200

    Изменен размер картинуи с линиями для десктопа во всех секциях

--- assets/dist/js/home.js
+++ assets/dist/js/home.js
@@ -3,11 +3,11 @@ $(document).ready(function () {
    var carouselSteps = $('.steps__carousel');
 
    carouselSteps.on("init", function (event, slick) {
-      $(".step-count").html(parseInt(slick.currentSlide + 1) + '<span> /' + slick.slideCount + '</span>');
+      $(".step-count--steps").html(parseInt(slick.currentSlide + 1) + '<span> /' + slick.slideCount + '</span>');
    });
 
    carouselSteps.on("afterChange", function (event, slick, currentSlide) {
-      $(".step-count").html(parseInt(slick.currentSlide + 1) + '<span> /' + slick.slideCount + '</span>');
+      $(".step-count--steps").html(parseInt(slick.currentSlide + 1) + '<span> /' + slick.slideCount + '</span>');
    });
 
 
@@ -19,19 +19,60 @@ $(document).ready(function () {
       responsive: [
 
          {
-            breakpoint: 768,
+            breakpoint: 767,
             settings: {
                slidesToShow: 2
             }
          },
          {
-            breakpoint: 1024,
+            breakpoint: 1023,
             settings: {
                slidesToShow: 3
             }
          },
          {
-            breakpoint: 1430,
+            breakpoint: 1339,
+            settings: "unslick"
+         }
+      ]
+   });
+
+
+
+
+
+   var carouseladvantages = $('.our-advantages__carousel');
+
+   carouseladvantages.on("init", function (event, slick) {
+      $(".step-count--our-advantages").html(parseInt(slick.currentSlide + 1) + '<span> /' + slick.slideCount + '</span>');
+   });
+
+   carouseladvantages.on("afterChange", function (event, slick, currentSlide) {
+      $(".step-count--our-advantages").html(parseInt(slick.currentSlide + 1) + '<span> /' + slick.slideCount + '</span>');
+   });
+
+
+   carouseladvantages.slick({
+      prevArrow: $('.our-advantages__arrow--prev'),
+      nextArrow: $('.our-advantages__arrow--next'),
+      dots: false,
+      mobileFirst: true,
+      responsive: [
+
+         {
+            breakpoint: 767,
+            settings: {
+               slidesToShow: 2
+            }
+         },
+         {
+            breakpoint: 1023,
+            settings: {
+               slidesToShow: 3
+            }
+         },
+         {
+            breakpoint: 1339,
             settings: "unslick"
          }
       ]
