b6a2f876d35509c493b1f1fa751c3979aa4dcd75
Glazebnaya-Kristina <46931114+Glazebnaya-Kristina@users.noreply.github.com>
2020-01-16 17:49:56 +0200

    Верстка advantages__carousel на странице advantages

--- assets/dist/js/advantages.js
+++ assets/dist/js/advantages.js
@@ -1,32 +1,38 @@
 $(document).ready(function () {
-   var carouseladvantages = $('.advantages__carousel');
+   var carouseladvantages2 = $('.advantages__carousel');
 
-   carouseladvantages.on("init", function (event, slick) {
+   carouseladvantages2.on("init", function (event, slick) {
       $(".step-count--advantages").html(parseInt(slick.currentSlide + 1) + '<span> /' + slick.slideCount + '</span>');
    });
 
-   carouseladvantages.on("afterChange", function (event, slick, currentSlide) {
+   carouseladvantages2.on("afterChange", function (event, slick, currentSlide) {
       $(".step-count--advantages").html(parseInt(slick.currentSlide + 1) + '<span> /' + slick.slideCount + '</span>');
    });
 
 
-   carouseladvantages.slick({
+   carouseladvantages2.slick({
       prevArrow: $('.advantages__arrow--prev'),
       nextArrow: $('.advantages__arrow--next'),
       dots: false,
+      infinite: true,
       mobileFirst: true,
       responsive: [
-
          {
             breakpoint: 767,
             settings: {
-               slidesToShow: 2
+               slidesToShow: 3,
+               centerMode: true,
+               variableWidth: true
             }
-         },
-         {
-            breakpoint: 1022,
-            settings: "unslick"
          }
+         // {
+         //    breakpoint: 1022,
+         //    settings: {
+         //       slidesToShow: 3,
+         //       centerMode: true,
+         //       variableWidth: true
+         //    }
+         // }
       ]
    });
 });
